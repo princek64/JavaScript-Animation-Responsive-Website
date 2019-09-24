b5cf4cd005d493f79bdb17f3f915f291e7a09d95
princek64 <princekukreja64@gmail.com>
2019-09-24 19:21:10 +0530

    GSAP Animation

---
+++ js/app.js
@@ -0,0 +1,56 @@
+function init(){
+    const slides = document.querySelectorAll('.slide')
+    const pages = document.querySelectorAll('.page');
+    const backgrounds = [
+        `radial-gradient(#2B3760, #0B1023)`,
+        `radial-gradient(#4E3022, #161616)`,
+        `radial-gradient(#4E4342, #161616)`
+    ];
+    
+    //Tracker
+    let current = 0;
+
+    slides.forEach((slide, index) => {
+        slide.addEventListener('click', function(){
+            changeDots(this);
+            nextSlide(index);
+        })
+    });
+
+    function changeDots(dot){
+        slides.forEach(slide => {
+            slide.classList.remove('active')
+        });
+        dot.classList.add('active');
+    }
+
+
+    function nextSlide(pageNumber){
+        const nextPage = pages[pageNumber];
+        const currentPage = pages[current]; 
+        const nextLeft = nextPage.querySelector('.hero .model-left');
+        const nextRight = nextPage.querySelector('.hero .model-right');
+        const currentLeft = currentPage.querySelector('.hero .model-left');
+        const currentRight = currentPage.querySelector('.hero .model-right');
+        const nextText = nextPage.querySelector('.details');
+        const portfolio = document.querySelector('.portfolio');
+
+        const tl = new TimelineMax();
+        
+        tl.fromTo(currentLeft, 0.3, {y: '-10%'}, {y: '-100%'})
+        .fromTo(currentRight, 0.3, {y: '10%'}, {y: '-100%'}, '-=0.2')
+        .to(portfolio, 0.3, {backgroundImage: backgrounds[pageNumber]})
+        .fromTo(currentPage, 0.3, {opacity: 1, pointerEvents: 'all'}, {opacity: 0, pointerEvents: 'none'})
+        .fromTo(nextPage, 0.3, {opacity: 0, pointerEvents: 'none'}, {opacity: 1, pointerEvents: 'all'}, "-=0.6")
+        .fromTo(nextLeft, 0.3, {y: '-100%'}, {y: '-10%'}, '-=0.6')
+        .fromTo(nextRight, 0.3, {y: '-100%'}, {y: '10%'}, '-=0.8')
+        .fromTo(nextText, 0.3, {opacity: 0, y: 30}, {opacity: 1, y: 0})
+        .set(nextLeft, {clearProps: 'all'})
+        .set(nextRight, {clearProps: 'all'})
+
+        current = pageNumber;
+
+    }
+}
+
+init();
